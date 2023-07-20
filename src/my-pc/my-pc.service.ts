5c4ead2669c502367c4eda944ec1eaa9205278ea
WilberthGamboa <wgcampos15@gmail.com>
2023-07-19 20:40:12 -0600

    inyectando modelo

--- src/my-pc/my-pc.service.ts
+++ src/my-pc/my-pc.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMyPcDto } from './dto/create-my-pc.dto';
 import { UpdateMyPcDto } from './dto/update-my-pc.dto';
+import { Model } from 'mongoose';
+import { User } from 'src/auth/entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { MyPc } from './entities/my-pc.entity';
 
 @Injectable()
 export class MyPcService {
+    constructor(
+        @InjectModel(User.name)
+        private readonly userModel: Model<User>,
+        @InjectModel(MyPc.name)
+        private readonly myPcModel: Model<MyPc>
 
-    submitMyPc(formData: CreateMyPcDto){
 
+    ){
+
+    }
+
+    submitMyPc(createMyPcDto: CreateMyPcDto){
+        
     }
 
 }
