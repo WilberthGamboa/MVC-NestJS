72e3c7eb88cc51dc5ee7b9ae4ad9ab163904daa5
WilberthGamboa <wgcampos15@gmail.com>
2023-08-05 17:11:59 -0600

    init implementacion

--- src/my-pc/dto/create-my-pc.dto.ts
+++ src/my-pc/dto/create-my-pc.dto.ts
@@ -1,12 +1,20 @@
-import { Allow, IsNotEmpty, IsNotEmptyObject, IsString } from 'class-validator';
+import { Allow, IsNotEmpty, IsNotEmptyObject, IsString, ValidationArguments } from 'class-validator';
+import { MemoryStore } from 'express-session';
+import { HasMimeType, IsFile, MemoryStoredFile } from 'nestjs-form-data';
 
 
 export class CreateMyPcDto {
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({message:'El nombre es obligatorio'})
   nombre: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({message:'La descripción es obligatoria'})
   descripcion: string;
+  @HasMimeType(['image/jpeg', 'image/png','image/webp'],{message : (validationArguments:ValidationArguments) =>{
+    return  `La imagen debe tener los siguientes formatos: ${validationArguments.constraints.toString()}`
+  }})
+  @IsFile({message:"La imagen es obligatoria"})
+ 
+  file:MemoryStoredFile;
 }
